8cd10592339e4304ec0da8189b10b8c95ea3ed53
Omar Richardson <omsrichardson@gmail.com>
2018-04-05 20:20:37 +0200

    Added file downloader

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # sandboxing
 
+*Playing with road network data*
+
+## What it does
+
+Not so much at the moment. It downloads traffic data and processes this into a pandas dataframe.
+But who is to say it will not solve traffic jams in a few weeks?
+
+## Running the code
+
+### Requirements
+ * Python3
+ * pandas
+ * numpy
+
+### Instructions
+
+Clone the repository. Before running any of the files, download the data with `python3 download.py`.
+
+## Other sources of information
+
+A master's thesis that uses the same data as a validation for new road network analysis techniques can be [here](https://dspace.library.uu.nl/bitstream/handle/1874/334224/Thesis%20-%20Johan%20Meppelink%20-%202016-05-17.pdf?sequence=2) (pdf)
---
+++ download_data.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+This script downloads the latest travel times and if not present, the measuring data from NDW and unpacks these files in the 'data' folder.
+"""
+import urllib.request
+import gzip
+import zipfile
+import shutil
+import os
+
+travel_time_url = "http://opendata.ndw.nu/traveltime.xml.gz"
+measuring_locations_url = "http://www.ndw.nu/downloaddocument/4ceb5142ed9e79bad0db5382736c474a/MST.zip"
+
+travel_time_file = "data/traveltime.xml"
+measuring_locations_file = "data/measuring_locations.xml"
+
+
+def download(url, file_name, overwrite):
+    """
+    Downloads gzipped files from the internet and unpacks them.
+
+    :param url: URL to the file
+    :param file_name: the name the downloaded file gets
+    :param overwrite: True if file should be overwritten, False otherwise
+    :return: None
+    """
+    ext = ".comp"
+    os.makedirs("data", exist_ok=True)
+    if os.path.exists(file_name):
+        if overwrite:
+            print("Overwriting %s" % file_name)
+        else:
+            print("%s exists, not overwriting" % file_name)
+            return
+    print("Downloading from %s" % url)
+    urllib.request.urlretrieve(url, file_name + ext)
+    print("Unpacking in %s" % file_name)
+    if url.endswith('.zip'):
+        zip_ref = zipfile.ZipFile(file_name + ext, 'r')
+        zip_ref.extractall('data')
+        zip_ref.close()
+    elif url.endswith('.gz'):
+        with gzip.open(file_name + ext, "rb") as f_in:
+            with open(file_name, "wb") as f_out:
+                shutil.copyfileobj(f_in, f_out)
+    os.remove(file_name + ext)
+
+
+if __name__ == "__main__":
+    download(travel_time_url, travel_time_file, overwrite=True)
+    download(measuring_locations_url, measuring_locations_file, overwrite=False)
