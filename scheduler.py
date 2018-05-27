93527ce6ae1e8f1930f75e2b69b5a331b831672c
lfasting <lfasting@NL5CG6517PJ7>
2018-05-27 12:07:22 +0200

    Schedules every minute for x times

--- scheduler.py
+++ scheduler.py
@@ -7,11 +7,11 @@ scheduler = sched.scheduler(time.time, time.sleep)
 def print_event(name):
     print("EVENT:", time.time(), name)
     os.system("python download_data.py")
+	
+for x in range(0, 5):
+    scheduler.enter(60 * x, 1, print_event, (x,))
 
 print("START:", time.time())
-scheduler.enter(5, 1, print_event, ('first',))
-scheduler.enter(65, 1, print_event, ('second',))
-scheduler.enter(125, 1, print_event, ('third',))
 
 scheduler.run()
 
