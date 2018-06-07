28045a5c908286238c2d2aa20f3ff04172fb3ff4
0mar <omsrichardson@gmail.com>
2018-06-07 08:53:38 +0200

    Fixed bug

--- scheduler.py
+++ scheduler.py
@@ -27,7 +27,7 @@ def parse_and_append(file_name):
     df = filter_df_on_locations(full_df, "Tilburg", "Eindhoven")
     debug("Filtered to %d entries" % len(df))
     debug("Appending data to dataframe")
-    old_df = pd.read_csv(file_name)
+    old_df = pd.read_csv(file_name, sep=';')
     previous_time = pd.to_datetime(old_df['timestamp'].loc[old_df['timestamp'].last_valid_index()])
     next_time = pd.to_datetime(df['timestamp'].loc[df['timestamp'].first_valid_index()])
     debug("Timestamp of previous measurements: %s" % previous_time)
@@ -37,7 +37,7 @@ def parse_and_append(file_name):
         return
     new_df = pd.concat([old_df, df])
     debug("New dataframe size: %d rows, %d cols\n" % new_df.shape)
-    new_df.to_csv(file_name, index_label=False)
+    new_df.to_csv(file_name, index=False, sep=';')
 
 
 def collect_data(minutes, file_id):
@@ -52,11 +52,9 @@ def collect_data(minutes, file_id):
     print("Storing dataframe in '%s'" % file_name)
     if os.path.exists(file_name):
         print("*Warning*: Overwriting %s" % file_name)
-    with open(file_name, 'w'):
-        pd.DataFrame().to_csv(file_name, index_label=False)  # Just for clearing the file
     print("Starting downloader at %s, scheduled for %d minutes" % (datetime.now().strftime("%d-%m-%y %H:%M"), minutes))
     download_all()
-    filter_df_on_locations(parse_to_dataframe(), "Tilburg", "Eindhoven").to_csv(file_name, index_label=False)
+    filter_df_on_locations(parse_to_dataframe(), "Tilburg", "Eindhoven").to_csv(file_name, index=False,sep=";")
     scheduler = sched.scheduler(time.time, time.sleep)
     for minute in range(1, minutes):
         scheduler.enter(60 * minute, 1, parse_and_append, (file_name,))
